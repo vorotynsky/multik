567e363defac27b753bfcfa1910f0e0c7c2f2afe
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2020-03-06 20:17:23 +0300

    chore: setup cmake project

---
+++ multik/main.cpp
@@ -0,0 +1,33 @@
+#define GLEW_STATIC
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+
+const int HEIGHT = 480;
+const int WIDTH = 640;
+
+int main(const int argc, const char **argv)
+{	
+	// //Инициализация GLFW
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Multik", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+
+	return 0;
+}
