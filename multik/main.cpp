1e4ba10cd0443cb036ad867ab145d977392bd721
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2020-03-22 16:51:13 +0300

    test: square with shader
    This code shows green square. It's should be refactored with additions
    of new features.

--- multik/main.cpp
+++ multik/main.cpp
@@ -1,3 +1,17 @@
+// Copyright 2020 vorotynsky
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -7,26 +21,133 @@
 const int HEIGHT = 480;
 const int WIDTH = 640;
 
+static unsigned int CompileShader(unsigned int type, const std::string &src)
+{
+    unsigned int shader = glCreateShader(type);
+    const char *ps = src.c_str();
+    glShaderSource(shader, 1, &ps, nullptr);
+    glCompileShader(shader);
+
+    int result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int len;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+        char *message = new char[len + 1];
+        glGetShaderInfoLog(shader, len, &len, message);
+        std::cerr << "compile shader error: " << message << std::endl;
+        delete[] message;
+    }
+
+    return shader;
+}
+
+static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
+static unsigned int LoadShader(const std::string &vertexPath, const std::string &fragmentPath)
+{
+    std::ifstream vertexFile(vertexPath);
+    std::string vstr((std::istreambuf_iterator<char>(vertexFile)),
+                    std::istreambuf_iterator<char>());
+
+    std::ifstream fragmentFile(fragmentPath);
+    std::string fstr((std::istreambuf_iterator<char>(fragmentFile)),
+                    std::istreambuf_iterator<char>());
+
+    unsigned int result = CreateShader(vstr, fstr);
+    vertexFile.close();
+    fragmentFile.close();
+    return result;
+}
+
+
 int main(const int argc, const char **argv)
-{	
-	glfwInit();
+{    
+    glfwInit();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Multik", NULL, NULL);
-	if (window == NULL)
-		return 1;
-	
-	glfwMakeContextCurrent(window);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Multik", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
+    glfwMakeContextCurrent(window);
 
-	while(!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-	}
-	glfwTerminate();
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-	return 0;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    unsigned int program =
+        LoadShader("shaders/square/color.vertex.glsl", "shaders/square/color.fragment.glsl");
+    glUseProgram(program);
+
+    float vertices[] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
+    unsigned int indexes[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    GLuint vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    unsigned int buffer, indexBuffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (GLvoid*)nullptr);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indexes, GL_STATIC_DRAW);
+
+    glBindVertexArray(vertexArray);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+
+        glClearColor(0.255f, 0.063f, 0.127f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        glfwSwapBuffers(window);
+    }
+    glDeleteProgram(program);
+    glDeleteVertexArrays(1, &vertexArray);
+    glDeleteBuffers(1, &buffer);
+
+    glfwTerminate();
+
+    return 0;
 }
